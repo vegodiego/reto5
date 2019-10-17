7a2d04af9e0d7ee6098edc0c4ff80f293e48d49b
vegodiego <53877309+vegodiego@users.noreply.github.com>
2019-10-16 23:09:29 -0500

    Update reto5.rb

--- reto5.rb
+++ reto5.rb
@@ -1,52 +1,52 @@
 class Jugadores
-	def adivinar(defEspec,respEspec)
-		puts
-		puts "definicion"
-		puts
-		puts defEspec
-		puts
-		entradaValida = false
-		while entradaValida == false
-			print "Adivinar: "
-			adivinar = gets.chomp
-			if adivinar == respEspec
-				entradaValida = true
-				puts
-				puts "Correcto!"
-			else
-				puts
-				puts "Incorrecto!, Trata de nuevo"
-				puts
-			end
-		end
-	end
+    def adivinar(defEspec,respEspec)
+        puts
+        puts "definicion"
+        puts
+        puts defEspec
+        puts
+        entradaValida = false
+        while entradaValida == false
+            print "Adivinar: "
+            adivinar = gets.chomp
+            if adivinar == respEspec
+                entradaValida = true
+                puts
+                puts "Correcto!"
+            else
+                puts
+                puts "Incorrecto!, Trata de nuevo"
+                puts
+            end
+        end
+    end
 
-	def jugar(listaDefin,listaResp)
-		puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
-		contador = 0
-		listaDefin.each do |i|
-			self.adivinar(i,listaResp[contador])
-			contador+=1
-		end
-		puts
-		puts "Juego finalizado"
-	end
+    def jugar(listaDefin,listaResp)
+        puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+        contador = 0
+        listaDefin.each do |i|
+            self.adivinar(i,listaResp[contador])
+            contador+=1
+        end
+        puts
+        puts "Juego finalizado"
+    end
 end
 
 class Respuestas
-	attr_accessor :resp
+    attr_accessor :resp
 
-	def initialize(resp)
-		@resp = resp.map {|i| i.gsub(" (Respuesta)","")}
-	end
+    def initialize(resp)
+        @resp = resp.map {|i| i.gsub(" (Respuesta)","")}
+    end
 end
 
 class Definiciones
-	attr_accessor :defin
+    attr_accessor :defin
 
-	def initialize(defin)
-		@defin = defin.map {|i| i.gsub(" (Definicion)","")}
-	end
+    def initialize(defin)
+        @defin = defin.map {|i| i.gsub(" (Definicion)","")}
+    end
 end
 
 
